eb0e738dc41681ff307c4bf557687506f03fb58b
yeong1110 <a0257942@kobiznet.co.kr>
2026-07-14 14:24:12 +0900

    style: 스타일 오류 변경

--- apps/public/src/main.tsx
+++ apps/public/src/main.tsx
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import "./styles.css";
+import "./select.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
