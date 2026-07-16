a6f777dd183e40b73f3b5763b9edd1c7cd58bf26
yeong1110 <a0257942@kobiznet.co.kr>
2026-07-16 18:32:20 +0900

    fix: bug 및 스타일오류 수정

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,2 +1,2 @@
-import React from "react"; import ReactDOM from "react-dom/client"; import { BrowserRouter } from "react-router-dom"; import { AdminApp } from "./AdminApp"; import "./styles.css"; import "./select.css";
+import React from "react"; import ReactDOM from "react-dom/client"; import { BrowserRouter } from "react-router-dom"; import { AdminApp } from "./AdminApp"; import "./styles.css"; import "./select.css"; import "./feedback.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><BrowserRouter><AdminApp/></BrowserRouter></React.StrictMode>);
